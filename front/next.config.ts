61bb5ef79d7fa7d9d33d492429c32162c6bebcf7
teppeiOno <lemmte2moka@gmail.com>
2026-04-08 18:25:18 +0900

    ✨:feat:css設定変更

--- front/next.config.ts
+++ front/next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
+  // process.cwd() = current working directory = project/front
+  sassOptions: {
+    prependData: `@use "${path.resolve(process.cwd(), "src/styles/global")}" as *;\n`,
+  },
 };
-
 export default nextConfig;
